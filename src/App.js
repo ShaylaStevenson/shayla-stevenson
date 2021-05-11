c33575a3ae60bc399bbc563c63c6287ef607954f
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-05-10 18:20:47 -0700

    fixed unwanted work, restored

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // ref unit 20 activity 4, 14
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 //import Project from "./components/Project";
 import Navbar from "./components/Navbar";
+import ProjectDetails from "./pages/ProjectDetails"
 
 function App() {
   document.title = "Shayla Stevenson"
@@ -16,9 +17,12 @@ function App() {
       <div>
         <Navbar />
         <Header />
-        <Route exact path="/" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/portfolio" component={Portfolio} />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/projects/:id" component={ProjectDetails} />
+        </Switch>
         <Footer />
       </div>
     </Router>
