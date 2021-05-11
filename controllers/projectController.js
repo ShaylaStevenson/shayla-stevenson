79e12b862f78d043650d2f303a352f489523a385
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-05-10 18:42:16 -0700

    added project controller file

---
+++ controllers/projectController.js
@@ -0,0 +1,17 @@
+// ref unit 20 mini-project
+const db = require("../models");
+
+// Defining methods for the postsController
+module.exports = {
+  findAll: function(req, res) {
+    db.Post.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Post.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
