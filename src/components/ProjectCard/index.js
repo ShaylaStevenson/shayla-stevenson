fef60043d2841c9de3497212b4d815c7b7682916
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-05-15 22:15:00 -0700

    refined card

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -5,30 +5,27 @@ import "./style.css";
 function ProjectCard(props) {
   const image = props.image
   return (
-    <div className="col-md-6">
-    <div className="card">
-      <div style={{backgroundImage: "url(" + image + ")"}}>
-      {/* className="img-container"<img alt={props.image} src={props.image} /> */}
-      <div className="content"> 
-        
-          <div className="card-title">
-            <strong>{props.title}</strong>
-          </div>
-          <p className="card-text">
-            <strong>{props.details}</strong>
-          </p>
-          <p className="card-text">
-            <a href={props.liveUrl}><strong>Go to Live Deployment</strong></a>
-          </p>
-          <p className="card-text">
-            <a href={props.ghRepoUrl}><strong>Visit Github Repository</strong></a>
-          </p>
-
-        
-      </div>
-      </div>
+    <div className="col-md-4">
+      <div className="card my-1" style={{backgroundImage: "url(" + image + ")", height: "205px"}}>
       
-    </div>
+        {/* className="img-container"<img alt={props.image} src={props.image} /> */}
+          <div className="content opaq-bg m-4"> 
+              <div className="card-title">
+                <strong>{props.title}</strong>
+              </div>
+              <p className="card-text">
+                <strong>{props.details}</strong>
+              </p>
+              <p className="card-text">
+                <a href={props.liveUrl}><strong>Go to Live Deployment</strong></a>
+              </p>
+              <p className="card-text">
+                <a href={props.ghRepoUrl}><strong>Visit Github Repository</strong></a>
+              </p>
+          </div>
+    
+        
+      </div>
     </div>
    );
 }
