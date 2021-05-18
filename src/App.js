934329a21e80cca81cdd331cd2a2f61daac4ee63
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-05-18 13:03:35 -0700

    footer and styling added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 import './index.css';
 
+
+
 const App = () => <PortfolioContainer />;
 
 export default App;
