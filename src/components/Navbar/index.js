18655b679c2d879e074ffa4162a2f67f44826316
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-05-18 14:30:13 -0700

    navbar style added plus testing body

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,65 +1,54 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "./style.css";
-
-// // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-// function Navbar() {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//       <Link className="navbar-brand" to="/">
-//         Shayla Stevenson
-//       </Link>
-//       <div>
-//         <ul className="navbar-nav">
-//           <li className="nav-item">
-//             <Link
-//               to="/"
-//               className={
-//                 window.location.pathname === "/" || window.location.pathname === "/about"
-//                   ? "nav-link active"
-//                   : "nav-link"
-//               }
-//             >
-//               About
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link
-//               to="/contact"
-//               className={
-//                 window.location.pathname === "/contact"
-//                   ? "nav-link active"
-//                   : "nav-link"
-//               }
-//             >
-//               Contact
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link
-//               to="/portfolio"
-//               className={
-//                 window.location.pathname === "/portfolio"
-//                   ? "nav-link active"
-//                   : "nav-link"
-//               }
-//             >
-//               Portfolio
-//             </Link>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
 import React from "react";
+import "./style.css";
 
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    //   <div className="navbar-brand">
+    //     <a
+    //       href="#about"
+    //       onClick={() => props.handlePageChange("About")}
+    //       className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Shayla Stevenson
+    //     </a>
+    //    </div>
+    //    <div>
+
+       
+    // <ul className="nav nav-tabs">
+    //   <li className="nav-item">
+    //     <a
+    //       href="#about"
+    //       onClick={() => props.handlePageChange("About")}
+    //       className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+    //     >
+    //       About
+    //     </a>
+    //   </li>
+    //   <li className="nav-item">
+    //     <a
+    //       href="#portfolio"
+    //       onClick={() => props.handlePageChange("Portfolio")}
+    //       className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Portfolio
+    //     </a>
+    //   </li>
+    //   <li className="nav-item">
+    //     <a
+    //       href="#contact"
+    //       onClick={() => props.handlePageChange("Contact")}
+    //       className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Contact
+    //     </a>
+    //   </li>
+    // </ul>
+    // </div>
+    //    </nav>
+
+    <nav className="navbar navbar-expand-sm navbar-dark fixed-top">
       <div className="navbar-brand">
         <a
           href="#about"
@@ -68,51 +57,100 @@ function Navbar(props) {
         >
           Shayla Stevenson
         </a>
-       </div>
-       <div>
 
-       
-    <ul className="nav nav-tabs">
-      {/* <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => props.handlePageChange("Home")}
-          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Home
-        </a>
-      </li> */}
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => props.handlePageChange("About")}
-          className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => props.handlePageChange("Portfolio")}
-          className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => props.handlePageChange("Contact")}
-          className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+        
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+             <span className="navbar-toggler-icon"></span>
+         </button>
+      </div>
+
+    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">  
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => props.handlePageChange("About")}
+            className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#portfolio"
+            onClick={() => props.handlePageChange("Portfolio")}
+            className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => props.handlePageChange("Contact")}
+            className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
     </div>
-       </nav>
+  </nav>
   );
 }
 
 export default Navbar;
+
+// <!--Navigation Bar-->
+    // <nav className="navbar navbar-expand-sm navbar-dark fixed-top">
+    //     {/* <a className="navbar-brand" href="/index.html"> */}
+    //         {/* <!-- <img src="" width="30" height="30" className="d-inline-block align-top" alt=""> --> */}
+    //         {/* Shayla Stevenson */}
+    //     {/* </a> */}
+    //     <a
+    //       href="#about"
+    //       onClick={() => props.handlePageChange("About")}
+    //       className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+    //     >
+    //       Shayla Stevenson
+    //     </a>
+
+    //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    //         <span className="navbar-toggler-icon"></span>
+    //     </button>
+
+    //     <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+    //         <ul className="navbar-nav">
+    //             <li className="nav-item active">
+    //                 {/* <a className="nav-link" href="index.html">Home</a> */}
+    //                 <a
+    //                   href="#about"
+    //                   onClick={() => props.handlePageChange("About")}
+    //                   className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+    //                 >
+    //                   About
+    //                 </a>
+    //             </li>
+    //             <li className="nav-item active">
+    //                 {/* <a className="nav-link" href="portfolio.html">Portfolio</a> */}
+    //                 <a
+    //                   href="#portfolio"
+    //                   onClick={() => props.handlePageChange("Portfolio")}
+    //                   className={props.currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
+    //                 >
+    //                   Portfolio
+    //                 </a>
+    //             </li>
+    //             <li className="nav-item active">
+    //                 {/* <a className="nav-link" href="portfolio.html">Portfolio</a> */}
+    //                 <a
+    //                   href="#contact"
+    //                   onClick={() => props.handlePageChange("Contact")}
+    //                   className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+    //                 >
+    //                   Contact
+    //                 </a>
+    //             </li>
+    //         </ul>
+    //     </div>  
+    // </nav>
