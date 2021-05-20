88c66a54ce0a5018404560c9ff402620521c72b4
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-05-19 19:08:02 -0700

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-//ref unit 19 act 21
-import React from "react";
-import MainContainer from "./components/MainContainer";
-import './index.css';
-import './App.css';
-
-const App = () => <MainContainer />;
-
-export default App;
